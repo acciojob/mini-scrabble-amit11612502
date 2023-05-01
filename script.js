5cfe8f8367d12fb006dbd26735dfcfef439377a0
amit11612502 <38473703+amit11612502@users.noreply.github.com>
2023-05-01 12:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let a= document.getElementById("evaluatedText");
+let b= document.getElementById("letterCount");
+
+a.addEventListener('inp', ()=> {
+	let str= a.value;
+	b.innerHTML= `${str.length}`;
+})
